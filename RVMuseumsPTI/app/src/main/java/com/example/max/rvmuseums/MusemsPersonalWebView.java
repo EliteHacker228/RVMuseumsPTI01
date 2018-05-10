bbc9543a5af75fcd89ba75e0654b36d8da590565
EliteHacker228 <maximusreg@yandex.ru>
2018-05-10 12:10:29 +0500

    Actual Version

--- RVMuseumsPTI/app/src/main/java/com/example/max/rvmuseums/MusemsPersonalWebView.java
+++ RVMuseumsPTI/app/src/main/java/com/example/max/rvmuseums/MusemsPersonalWebView.java
@@ -1,19 +1,31 @@
 package com.example.max.rvmuseums;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MusemsPersonalWebView extends AppCompatActivity {
 
     WebView webview;
+    String URL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_musems_personal_web_view);
         webview=findViewById(R.id.museums_webview);
+        setTitle("Веб-сайт музея");
+        Intent intent = getIntent();
+        URL=intent.getStringExtra("URL");
+        Log.d("URL loading: ", intent.getStringExtra("URL"));
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl(URL);
+
+        webview.setWebViewClient(new MyWebViewClient());
+
     }
 
     class MyWebViewClient extends WebViewClient {
--- RVMuseumsPTI/app/src/main/java/com/example/max/rvmuseums/MuseumActivity.java
+++ RVMuseumsPTI/app/src/main/java/com/example/max/rvmuseums/MuseumActivity.java
@@ -4,11 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +23,8 @@ import java.net.URL;
 public class MuseumActivity extends AppCompatActivity {
 
     TextView name, website, trivia, adress, phone;
+    ConstraintLayout museumContainer;
+    LinearLayout museumInformContainer;
     ImageView poster;
 
 
@@ -39,16 +43,12 @@ public class MuseumActivity extends AppCompatActivity {
         adress=findViewById(R.id.adress);
         phone=findViewById(R.id.phone);
         poster = findViewById(R.id.poster);
+        museumContainer=findViewById(R.id.museum_container);
+        museumInformContainer=findViewById(R.id.museum_inf_container);
 
 
-        class WebsiteClicker implements View.OnClickListener{
 
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Скоро сделаю переход на сайты", Toast.LENGTH_SHORT).show();
-            }
-        }
-        website.setOnClickListener(new WebsiteClicker());
+
 
 
         name.setText(intent.getStringExtra("name"));
@@ -56,6 +56,15 @@ public class MuseumActivity extends AppCompatActivity {
         adress.setText(intent.getStringExtra("adress"));
         website.setText(intent.getStringExtra("website"));
         phone.setText(intent.getStringExtra("phone"));
+
+        if(website.getText().equals("")){
+           museumInformContainer.removeView(website);
+        }else if(phone.getText().equals("")){
+            museumInformContainer.removeView(phone);
+        }else if(adress.getText().equals("неизвестна")){
+            museumInformContainer.removeView(adress);
+        }
+
         //Picasso.get().load(intent.getStringExtra("URL")).resize(640, 480).into(poster);
         if(intent.getStringExtra("URL").isEmpty()){
             Picasso.get().load("https://yt3.ggpht.com/a-/AJLlDp3w3Ok_TD46pLqIlFB7_TbbwUHQ4D867hKRhQ=s900-mo-c-c0xffffffff-rj-k-no").resize(640, 480).into(poster);
@@ -63,7 +72,23 @@ public class MuseumActivity extends AppCompatActivity {
         }else {
             Picasso.get().load(intent.getStringExtra("URL")).resize(640, 480).into(poster);
             //Log.d("NewsPosterLostGet", news.getNewsPicURL());
+
         }
+
+        class WebsiteClicker implements View.OnClickListener{
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MusemsPersonalWebView.class);
+                String message="http://"+website.getText();
+                intent.putExtra("URL", message);
+                Log.d("Load URL", message);
+                startActivity(intent);
+
+                //Toast.makeText(getApplicationContext(), "Скоро сделаю переход на сайты", Toast.LENGTH_SHORT).show();
+            }
+        }
+        website.setOnClickListener(new WebsiteClicker());
 
         setTitle("Музей");
 
